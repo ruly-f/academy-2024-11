e7f0b307e9991e0f4a0eb1d7dbcdb7366990ad62
Rulyan Fernandes <rulyan.fernandes@indicium.tech>
2025-05-23 08:42:43 +0000

    Translated repo to english

--- models/marts/dim_funcionarios.sql
+++
@@ -1,8 +0,0 @@
-with
-    dim_funcionarios as (
-        select *
-        from {{ ref('int_vendas__self_join_funcionarios') }}
-    )
-
-select *
-from dim_funcionarios
--- models/marts/fct_vendas.sql
+++
@@ -1,8 +0,0 @@
-with
-    fct_vendas as (
-        select *
-        from {{ ref('int_vendas__metricas_de_vendas') }}
-    )
-
-select *
-from fct_vendas
